037a3118287f5fcabde3f6b443485b9b582eda4b
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2024-02-22 23:33:34 +0530

    style: Update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './pages/home/HomePage';
+import { Navbar } from './components/navbar/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <HomePage/>
-  );
+    </>
+    );
 }
 
 export default App;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './navbar.css'
+export const Navbar = () => {
+  return (
+    <nav>
+        <h1>Book Shelf</h1>
+    </nav>
+  )
+}
